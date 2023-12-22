a46b21c065892beaa0d6bba9b53dc23c01f4f122
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2023-12-22 02:39:35 +0100

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 const isAndroid = process.env.CAPACITOR_PLATFORM === 'android';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'frontend-template',
+  appId: 'com.c7pixel.europa.the.celestial.quest',
+  appName: 'Europa: The Celestial Quest',
   webDir: 'dist',
   server: {
     androidScheme: isAndroid ? 'http' : 'https',
